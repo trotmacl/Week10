2a775a184f97d8d91d779849be55ecf85e0bdc7d
trotmacl <trotmacl@miamioh.edu>
2021-11-11 23:37:33 -0500

    Completes project

--- Week10/Assets/Scripts/SimpleNavMeshController.cs
+++ Week10/Assets/Scripts/SimpleNavMeshController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class SimpleNavMeshController : MonoBehaviour
 {
@@ -9,10 +10,13 @@ public class SimpleNavMeshController : MonoBehaviour
     private NavMeshAgent _agent;
     [SerializeField]
     private Camera _camera;
+
+    public ThirdPersonCharacter character;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _agent.updateRotation = false;
     }
 
     // Update is called once per frame
@@ -25,6 +29,14 @@ public class SimpleNavMeshController : MonoBehaviour
             {
                 _agent.SetDestination(hit.point);
             }
+        }
+
+        if (_agent.remainingDistance > _agent.stoppingDistance)
+        {
+            character.Move(_agent.desiredVelocity, false, false);
+        }
+        else {
+            character.Move(Vector3.zero, false, false);
         }
     }
 }
